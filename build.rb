5408c026ced487514878c8dfebfb0a7e168b40ed
Peter Yates <peter.yates@graphia.co.uk>
2024-06-18 16:18:12 +0100

    Convert eastings and northings to wgs_84 coords

--- build.rb
+++ build.rb
@@ -3,6 +3,7 @@ require 'date'
 require 'json'
 require 'net/http'
 require 'open-uri'
+require 'breasal'
 
 def gias_csv(base: 'https://ea-edubase-api-prod.azurewebsites.net/edubase/downloads/public/edubasealldata%s.csv')
   URI.open(sprintf(base, Date.today.strftime('%Y%m%d'))).read
@@ -12,17 +13,22 @@ def write_json(filename, hash)
   File.write(%(output/#{filename}.json), hash.to_json)
 end
 
+def easting_northing_to_wgs84(easting:, northing:)
+  return {} unless easting && northing
+
+  Breasal::EastingNorthing.new(easting:, northing:, type: :gb).to_wgs84
+end
+
 schools = CSV.parse(gias_csv, headers: true, encoding: 'ISO-8859-1:UTF-8').map.with_object([]) do |r, arr|
-  school = {
+  arr << {
     urn: r['URN'].to_i,
     name: r['EstablishmentName'],
-    local_authority_name: r['LA (name)'],
+    local_authority: r['LA (name)'],
     phase_of_education: r['PhaseOfEducation (name)'],
     gender: r['Gender (name)'],
     status: r['EstablishmentStatus (name)'],
+    **easting_northing_to_wgs84(easting: r["Easting"].to_f, northing: r["Northing"].to_f)
   }
-
-  arr << school
 end
 
 schools.each { |s| write_json(%(schools/#{s[:urn]}), s) }
